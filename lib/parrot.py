59b59abc71f6bebbb037c8c0dc3261caccdc26f2
ldv432 <ldv432@gmail.com>
2024-10-28 10:18:28 -0600

    parrot

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,10 @@
-def parrot():
-    pass
+def parrot(hello=None, default="Squawk!"):
+    if hello is None:
+        print(default)
+        return default
+    else:
+        print(hello)
+        return hello
+
+
+    
